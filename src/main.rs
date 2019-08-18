4a55438a7adeb1c11a60bcaaccb93a36cc580bea
jdlau <jdlau@qq.com>
2019-08-18 21:56:37 +0800

    添加方法

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("home.")
+}
+
+#[get("hello")]
+fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+     HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(hello)
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
 }
